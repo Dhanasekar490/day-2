3d5d8d2b57d4b22cb001ebd0521a1bade1d25895
vishmayamaki <maki83148@gmail.com>
2024-05-12 00:13:32 +0530

    first commit

---
+++ q1.js
@@ -0,0 +1,22 @@
+//write a blog on the difference between document and window object
+
+//answer:
+
+
+/* Document Object:
+
+Represents the structure and content of a web page.
+Provides methods to manipulate HTML elements and their attributes.
+Organizes elements in a hierarchical tree structure (DOM).
+
+Window Object:
+
+Represents the browser window or tab.
+Serves as the global context for JavaScript code.
+Provides methods to interact with the browser environment and control window behavior.
+
+
+Relationship:
+
+Document is a property of Window (window.document).
+Window methods affect the Document object or its elements.*/
